7072da57190d3d469c12bfce61ccb4dc84fe9e81
Erik Kristensen <erik@erikkristensen.com>
2014-12-08 20:31:10 -0700

    webhook support and new variables for controlling the index

--- config/docker.js
+++ config/docker.js
@@ -8,5 +8,12 @@ module.exports = {
     port: process.env.REDIS_PORT_6379_TCP_PORT || 6379,
     host: process.env.REDIS_PORT_6379_TCP_ADDR || 'localhost'
   },
-  registries: process.env.REGISTRIES.split(',')
+  registries: process.env.REGISTRIES.split(','),
+  webhooks: {
+    disabled: process.env.WEBHOOKS_DISABLED || false, // Globally disable the use of webhooks
+    timeout: process.env.WEBHOOKS_TIMEOUT || 3000,   // Time in milliseconds to wait for a successful webhook
+    history: process.env.WEBHOOKS_HISTORY || 30,     // Number of history records to keep per webhook (0 or false to disable)
+  },
+  disable_account_registration: process.env.DISABLE_ACCOUNT_REGISTRATION || false,
+  disable_new_accounts: process.env.DISABLE_NEW_ACCOUNTS || true,
 }
