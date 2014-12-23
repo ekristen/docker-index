31d863c48dcba825e77bce6b5cfa1577a95f7285
Erik Kristensen <erik@erikkristensen.com>
2014-12-23 07:09:59 -0700

    Update defaults.js

--- index/defaults.js
+++ index/defaults.js
@@ -8,7 +8,6 @@ module.exports = function(config, redis, logger) {
   endpoints.ping = function(req, res, next) {
     // TODO: pass through to the actual registry??
     if (typeof(config.test) != "undefined" && config.test == true) {
-      res.setHeader('X-Docker-Registry-Version', '0.8.0');
       res.setHeader('X-Docker-Registry-Standalone', 'false');
       res.send(200);
       return next();
@@ -19,20 +18,14 @@ module.exports = function(config, redis, logger) {
       request.get(url, function(err, response, body) {
         next.ifError(err);
 
-        if (typeof(response.headers['x-docker-registry-version']) == "undefined" || 
-            typeof(response.headers['x-docker-registry-standalone']) == "undefined") {
+        if (typeof(response.headers['x-docker-registry-standalone']) == "undefined") {
           logger.error('Unable to find required headers when connecting to the registry', response.headers);
           return next('Unable to find required headers when connecting to the registry');
         }
 
-        logger.debug(util.format('X-Docker-Registry-Version: %s', response.headers['x-docker-registry-version']));
         logger.debug(util.format('X-Docker-Registry-Standalone: %s', response.headers['x-docker-registry-standalone']));
-        logger.debug(util.format('X-Docker-Registry-Config: %s', response.headers['x-docker-registry-config']));
 
-        res.setHeader('X-Docker-Registry-Version', response.headers['x-docker-registry-version']);
         res.setHeader('X-Docker-Registry-Standalone', response.headers['x-docker-registry-standalone']);
-        res.setHeader('X-Docker-Registry-Config', response.headers['x-docker-registry-config'] || 'unknown');
-
         res.send(200);
         return next();
       }); 
